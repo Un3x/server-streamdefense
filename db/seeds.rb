dfa98cba833e06fbe9b37f9f4431f9509b11b39a
Thomas COMES <thomas.comes.tech@gmail.com>
2024-02-01 16:06:04 +0100

    enhanced seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,237 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-#
+# Admin
 User.create(
   email: 'admin@random.com',
   password: 'leRandomPourLesHommes',
   role: 'ADMIN'
 )
 
-User.create(
-  email: 'test@random.com',
-  password: 'leRandomPourLesHommes',
-  twitch_id: '123456789',
-  nickname: 'randomableman',
+# Structures
+
+hotel_de_ville = Structure.create(
+  name: 'Hotel de ville',
+  description: 'Ceci est un hotel de ville'
+)
+
+ferme = Structure.create(
+  name: 'Ferme',
+  description: 'Ceci est une ferme'
+)
+
+scierie = Structure.create(
+  name: 'Scierie',
+  description: 'Ceci est un scierie'
+)
+
+camp_militaire = Structure.create(
+  name: 'Camp militaire',
+  description: 'Ceci est un camp militaire'
+)
+
+# Structures requirements
+
+StructureRequirement.create(
+  structure: camp_militaire,
+  required_structure: scierie,
+  required_level: 2
+)
+
+StructureRequirement.create(
+  structure: camp_militaire,
+  required_structure: ferme,
+  required_level: 2
+)
+
+# Ressources
+
+bois = Resource.create(
+  name: 'Bois'
+)
+
+nourriture = Resource.create(
+  name: 'Nourriture'
+)
+
+fer = Resource.create(
+  name: 'Fer'
+)
+
+# Players
+
+reallord = User.create(
+  email: 'reallordk1@random.com',
+  password: 'RealLordK1',
+  twitch_id: '999999999',
+  nickname: 'RealLordK1',
   role: 'USER'
+)
+
+lavomancien = User.create(
+  email: 'lavomancien@random.com',
+  password: 'RealLordK1',
+  twitch_id: '888888888',
+  nickname: 'Lavomancien',
+  role: 'USER'
+)
+
+orbital = User.create(
+  email: 'orbital@random.com',
+  password: 'Orbital',
+  twitch_id: '777777777',
+  nickname: 'Orbital',
+  role: 'USER'
+)
+
+# Channels
+
+unex = Channel.create(
+  twitch_id: '000000000000'
+)
+
+# Games
+
+game_1 = IdleGame.create(
+  channel: unex,
+  user: reallord
+)
+
+game_2 = IdleGame.create(
+  channel: unex,
+  user: lavomancien
+)
+
+game_3 = IdleGame.create(
+  channel: unex,
+  user: orbital
+)
+
+# Games Ressources
+
+IdleGameResource.create(
+  idle_game: game_1,
+  resource: bois,
+  quantity: 100
+)
+
+IdleGameResource.create(
+  idle_game: game_1,
+  resource: nourriture,
+  quantity: 1000
+)
+
+IdleGameResource.create(
+  idle_game: game_1,
+  resource: fer,
+  quantity: 2000
+)
+
+IdleGameResource.create(
+  idle_game: game_2,
+  resource: bois,
+  quantity: 3000
+)
+
+IdleGameResource.create(
+  idle_game: game_2,
+  resource: nourriture,
+  quantity: 69
+)
+
+IdleGameResource.create(
+  idle_game: game_2,
+  resource: fer,
+  quantity: 70
+)
+
+IdleGameResource.create(
+  idle_game: game_3,
+  resource: bois,
+  quantity: 0
+)
+
+IdleGameResource.create(
+  idle_game: game_3,
+  resource: nourriture,
+  quantity: 0
+)
+
+IdleGameResource.create(
+  idle_game: game_3,
+  resource: fer,
+  quantity: 0
+)
+
+# Games Structures
+
+IdleGameStructure.create(
+  idle_game: game_1,
+  structure: hotel_de_ville,
+  level: 3
+)
+
+IdleGameStructure.create(
+  idle_game: game_1,
+  structure: ferme,
+  level: 2
+)
+
+IdleGameStructure.create(
+  idle_game: game_1,
+  structure: scierie,
+  level: 2
+)
+
+IdleGameStructure.create(
+  idle_game: game_1,
+  structure: camp_militaire,
+  level: 1
+)
+
+IdleGameStructure.create(
+  idle_game: game_2,
+  structure: hotel_de_ville,
+  level: 2
+)
+
+IdleGameStructure.create(
+  idle_game: game_2,
+  structure: ferme,
+  level: 1
+)
+
+IdleGameStructure.create(
+  idle_game: game_2,
+  structure: scierie,
+  level: 2
+)
+
+IdleGameStructure.create(
+  idle_game: game_2,
+  structure: camp_militaire,
+  level: 0
+)
+
+IdleGameStructure.create(
+  idle_game: game_3,
+  structure: hotel_de_ville,
+  level: 1
+)
+
+IdleGameStructure.create(
+  idle_game: game_3,
+  structure: ferme,
+  level: 0
+)
+
+IdleGameStructure.create(
+  idle_game: game_3,
+  structure: scierie,
+  level: 0
+)
+
+IdleGameStructure.create(
+  idle_game: game_3,
+  structure: camp_militaire,
+  level: 0
 )

58c670d8358c09fa9ad1ad84bdf3e9edebc42b67
Thomas COMES <thomas.comes.tech@gmail.com>
2024-10-30 16:25:02 +0100

    Added cgu and politique_confidentialite_url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@
 
 class HomeController < ApplicationController
   def index; end
+
+  def cgu; end
+
+  def politique_confidentialite; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,42 +12,57 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :seasons
+
     resources :structure_formulas do
       collection do
         post :import
       end
     end
+
     resources :structure_requirements do
       collection do
         post :import
       end
     end
+
     resources :idle_game_structures
+
     resources :structures do
       collection do
         post :import
       end
     end
+
     resources :idle_game_resources
+
     resources :resources do
       collection do
         post :import
       end
     end
+
     resources :idle_games, only: %i[index show destroy] do
       collection do
         delete :destroy_all
       end
     end
+
     resources :channels, only: %i[index show]
+
     resources :users, only: %i[index show]
+
     resources :trade_requests, only: %i[index show]
+
     get '/', to: 'home#index', as: :root
   end
 
   devise_for :users
 
   get 'up' => 'rails/health#show', as: :rails_health_check
+
+  get '/cgu', to: 'home#cgu', as: :cgu
+
+  get '/politique_confidentialite', to: 'home#politique_confidentialite', as: :politique_confidentialite
 
   root 'home#index'
 end

5858891d7c3af883e9ce035bb2d44e4cef976ab4
Thomas COMES <thomas.comes.tech@gmail.com>
2024-10-30 17:13:33 +0100

    Fixing redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 # rubocop:disable Style/GlobalVars
-$redis = Redis.new(url: ENV.fetch('REDIS_TLS_URL', nil), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+$redis = Redis.new(url: ENV.fetch('REDIS_URL', nil), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 # rubocop:enable Style/GlobalVars
